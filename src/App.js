83bd27d46fc17906bb09c54537c0f11e638274f1
henry-design <henrymacharia09@gmail.com>
2022-10-23 01:19:14 +0300

    particles added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Particle from "./components/configs/Particle"
+import particlesCon from './components/configs/particlesConfig'
 
 import Navbar from './components/Navbar'
+import Header from './components/Header'
 function App() {
-  return (
-    <div className="App">
+  return ( 
+    <div className="App"> 
+      <Particle />
   <Navbar />
+  <Header/>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import Typed from "react-typed"
+
+const Header = () =>{
+    
+
+    return (
+     <div className="header-wrapper">
+        <div className="main-info">
+            <h1>
+                Going the extra mile for your business...
+            </h1>
+            <Typed
+            className="typed-text"
+            strings={
+                ["Web Applications Design and Development","Software Design, Development, and Intergrations","Data analytics","Systems Administration"]
+            }
+            typeSpeed={50}
+                    backSpeed={20}
+                   
+                    loop 
+                   
+            />
+            <a href="#" className="btn-main-offer">Cotact me</a>
+        </div>
+     </div>
+    )
+}
+export default Header
